8b6ce7130518149b0e9ca30b771e9002586300e6
seetharamakash2026 <seetharamakash2026@serrahs.com>
2025-09-06 20:12:23 -0700

    add climbing

--- src/main/java/frc/robot/Constants/ClimberConstants.java
+++ src/main/java/frc/robot/Constants/ClimberConstants.java
@@ -6,7 +6,7 @@ public class ClimberConstants {
     public static final int pivotID = 20;
     public static final int wingsID = 16; // these might need to be switched around
 
-    public static final String canbus = "Canivore 3045";
+    public static final String CAN_STRING = "Canivore 3045";    
     public static final int pivotCancoderID = 20;
 
     public static final String climberTable = "climber";
@@ -20,9 +20,9 @@ public class ClimberConstants {
     public static final double maxAngle = 90; 
     public static final double minAngle = 0; 
 
-    public static final double pivotOffsetX = Units.inchesToMeters(0);
-    public static final double pivotOffsetY = Units.inchesToMeters(0);
-    public static final double pivotOffsetZ = Units.inchesToMeters(0);
+    public static final double pivotOffsetX = Units.inchesToMeters(12.978190);
+    public static final double pivotOffsetY = Units.inchesToMeters(0.280000);
+    public static final double pivotOffsetZ = Units.inchesToMeters(-12.594950);
     
     public static final double pivotRotorToSensorRatio = 1; // no idea how this is calculated
     public static final double pivotSensorToMechanismRatio = 0; 
@@ -30,5 +30,5 @@ public class ClimberConstants {
     public static final double pivotMOI = 0.0000000001; // probably make this a real value? 
     public static final double intakeLength = Units.inchesToMeters(0);
 
-    
+    public static final double climberLength = Units.inchesToMeters(11.354741);
 }
--- src/main/java/frc/robot/Subsystems/Climber.java
+++ src/main/java/frc/robot/Subsystems/Climber.java
@@ -1,33 +1,47 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commons.GremlinLogger;
 import frc.robot.commons.GremlinUtil;
 
-import static frc.robot.Constants.AlgaeIntakeConstants.CAN_STRING;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Volts;
+
 import static frc.robot.Constants.ClimberConstants.*;
-import static frc.robot.Constants.ElevatorPivotConstants.pivotSupplyCurrentLimitLowerLimit;
 
 import java.io.ObjectInputFilter.Config;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-/*
- * TO DO:
- * Entire claw system to actually climb commands and everything past periodic 
- * figure out how the claw works in shop then it'll be simple
- * 
- */
+import com.ctre.phoenix6.sim.CANcoderSimState;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
 public class Climber extends SubsystemBase {
     private TalonFX pivotMotor = new TalonFX(pivotID, CAN_STRING);
     private TalonFX wingsMotor = new TalonFX(wingsID, CAN_STRING);
@@ -49,7 +63,7 @@ public class Climber extends SubsystemBase {
         targetAngleDegrees = getAngleDegrees(); // define
     }
 
-    public void configDevides() {
+    public void configDevices() {
         pivotMotor.getConfigurator().apply(pivotMotorConfig); // define
         wingsMotor.getConfigurator().apply(wingMotorConfig); // define
         pivotCancoder.getConfigurator().apply(pivotCancoderConfig); // define
@@ -143,7 +157,7 @@ public class Climber extends SubsystemBase {
     }
     public Command applyUpwardCurrent() {
         return this.runOnce(() -> {
-            double current = -10;
+            double current = 10;
             pivotMotor.setControl(new TorqueCurrentFOC(current));
         });
     }
@@ -154,10 +168,151 @@ public class Climber extends SubsystemBase {
         });
     }
 
+    public Command intakeCage() {
+        return this.runOnce(() -> {
+            double current = 10;
+            wingsMotor.setControl(new TorqueCurrentFOC(current));
+        });
+    }
+    public Command ejectCage() {
+        return this.runOnce(() -> {
+            double current = -10;
+            wingsMotor.setControl(new TorqueCurrentFOC(current));
+        });
+    }
+    public Command stopWingsMotor() {
+        return this.runOnce(() -> {
+            wingsMotor.setControl(new TorqueCurrentFOC(0));
+        });
+    }
+
+    public Command beginClimbSequence() {
+        return this.runOnce(() -> 
+            max()
+            .andThen(intakeCage())
+        );
+    }
+
     @Override
     public void periodic() {
         updateMechanism2d(); // define 
         
         SmartDashboard.putNumber("pivot angle", getAngleDegrees());
+    }
+
+
+    // sim
+    private final FlywheelSim flywheelSim = new FlywheelSim(
+        LinearSystemId.createFlywheelSystem(DCMotor.getKrakenX60(1), 
+        flywheelMOI, // define
+        flywheelGearing), // define
+    DCMotor.getKrakenX60(1)); 
+
+    private final SingleJointedArmSim singleJointedArmSim = new SingleJointedArmSim(
+        DCMotor.getKrakenX60(1),
+        pivotGearing, // define
+        pivotMOI, // define
+        climberLength, // define
+        Units.degreesToRadians(minAngle),
+        Units.degreesToRadians(maxAngle),
+        true,
+        Units.degreesToRadians(minAngle)
+    ); 
+    private TalonFXSimState wingsMotorSim;
+    private TalonFXSimState pivotMotorSim;
+    private CANcoderSimState pivotCancoderSim; 
+
+    private Mechanism2d pivotMech = new Mechanism2d(canvasWidth, canvasHeight); // define both of these
+    private StructArrayPublisher<Pose3d> componentPosesPublisher = NetworkTableInstance.getDefault()
+        .getTable(climberTable)
+        .getStructArrayTopic("componentPoses", Pose3d.struct).publish();
+    
+    private MechanismRoot2d pivotRoot = pivotMech.getRoot(("pivotRoot"), 2, 3); // no clue what the numbers are
+    private MechanismLigament2d pivotLigament = pivotRoot.append(new MechanismLigament2d("climberLigament", climberLength, minAngle));
+    
+    public void configSimulation() {
+        wingsMotorSim = wingsMotor.getSimState();
+        pivotMotorSim = pivotMotor.getSimState();
+        pivotCancoderSim = pivotCancoder.getSimState();
+
+        wingsMotorSim.Orientation = ChassisReference.CounterClockwise_Positive; // maybe fix
+        pivotMotorSim.Orientation = ChassisReference.CounterClockwise_Positive;
+
+        singleJointedArmSim.setState(0,0);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        wingsMotorSim = wingsMotor.getSimState();
+        pivotMotorSim = pivotMotor.getSimState();
+        pivotCancoderSim = pivotCancoder.getSimState();
+
+        wingsMotorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+        pivotCancoderSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+        pivotMotorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+
+        flywheelSim.setInputVoltage(pivotMotorSim.getMotorVoltageMeasure().in(Volts));
+        flywheelSim.update(0.02);
+
+        singleJointedArmSim.setInputVoltage(pivotMotorSim.getMotorVoltageMeasure().in(Volts));
+        singleJointedArmSim.update(0.02);
+
+
+        pivotCancoderSim.setRawPosition(edu.wpi.first.math.util.Units.radiansToRotations((singleJointedArmSim.getAngleRads() * pivotSensorToMechanismRatio))); 
+        pivotCancoderSim.setVelocity(edu.wpi.first.math.util.Units.radiansToRotations(singleJointedArmSim.getVelocityRadPerSec() * pivotSensorToMechanismRatio)); 
+        pivotMotorSim.setRawRotorPosition(edu.wpi.first.math.util.Units.radiansToRotations(singleJointedArmSim.getAngleRads() * pivotTotalGearing));
+        pivotMotorSim.setRotorVelocity(edu.wpi.first.math.util.Units.radiansToRotations(singleJointedArmSim.getVelocityRadPerSec() * pivotTotalGearing));
+            
+        updateMechanism2d();
+    }
+
+    public void updateMechanism2d() {
+        double currentAngle = getAngleDegrees();
+
+        if (GremlinLogger.DEBUG) { // ON BY DEFAULT
+            pivotLigament.setAngle(180 - currentAngle); // I have a vague idea of what this is for
+
+            componentPosesPublisher.set(new Pose3d[] {
+                new Pose3d(pivotOffsetX, 
+                            pivotOffsetY, 
+                            pivotOffsetZ, 
+                            new Rotation3d(0, 
+                                            -getAngleRadians(), // same reason as the 180 - probably
+                                            0))
+            });
+        }
+
+        GremlinLogger.log("Climber/Pivot", new Pose3d[] {
+            new Pose3d(pivotOffsetX, 
+                        pivotOffsetY,
+                        pivotOffsetZ, 
+                        new Rotation3d(0, 
+                                        -getAngleRadians(), // same reason as the 180 - probably
+                                        0))
+        });
+    }
+
+    private final SysIdRoutine m_SysIdRoutine = new SysIdRoutine(
+        new SysIdRoutine.Config(
+            Volts.of(0.5).per(Second),
+            Volts.of(2),
+            null,
+            (state) -> SignalLogger.writeString("state", state.toString())
+        ),
+        new SysIdRoutine.Mechanism(
+            (volts) -> {
+                pivotMotor.setVoltage((volts.in(Volts)));
+            }, 
+            null, 
+            this)
+        );
+                
+        
+        // possibly redundant
+    public Command sysIdQuasistatic(SysIdRoutine.Direction d) {
+        return m_SysIdRoutine.quasistatic(d);
+    }
+    public Command sysIdDynamic(SysIdRoutine.Direction d) {
+        return m_SysIdRoutine.dynamic(d);
     }
 }
