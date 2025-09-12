bfcc4e4a25d1acbdb1bf85d1556debcb8a1f6d88
Bolb347 <mp2k20@icloud.com>
2025-09-11 17:02:00 -0700

    Elevator position calc

--- src/main/java/frc/robot/Subsystems/Elevator.java
+++ src/main/java/frc/robot/Subsystems/Elevator.java
@@ -8,12 +8,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.ElevatorConstants.*;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
   private double elevatorHeight;
   private double numRotations;
+
+  private TalonFX elevatorMotor1 = new TalonFX(MOTOR1ID, "Team3045");
+  private TalonFX elevatorMotor2 = new TalonFX(MOTOR2ID, "Team3045");
   
-  public Elevator() {}
+  public Elevator() {
+    numRotations = 0;
+    elevatorHeight = 0;
+  }
 
   private double calcHeightFromRotations(double pinnionRotations) {
     return pinnionRotations * PINNIONTODRUMRATIO * DRUMROTATIONTODISTANCE;
@@ -21,6 +29,8 @@ public class Elevator extends SubsystemBase {
 
   @Override
   public void periodic() {
+    numRotations = elevatorMotor1.getRotorPosition().getValueAsDouble();
+    elevatorHeight = calcHeightFromRotations(numRotations);
     // This method will be called once per scheduler run
   }
 }
